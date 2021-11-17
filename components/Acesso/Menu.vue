1f777fa1e8ad9a8dea29e4aacbd9ae0aa033b79e
joasmoura <40508788+joasmoura@users.noreply.github.com>
2021-11-16 21:29:58 -0300

    Update Menu.vue

--- components/Acesso/Menu.vue
+++ components/Acesso/Menu.vue
@@ -1,13 +1,13 @@
 <template>
-<div>
-  <b-nav vertical>
-    <b-nav-item v-bind:active="$route.path === '/conta'" to="/conta"><b-icon icon="disc" /> Mus cds</b-nav-item>
-    <b-nav-item v-bind:active="$route.path === '/conta/meus-dados'" to="/conta/meus-dados"><b-icon icon="pencil-square" /> Meus dados</b-nav-item>
-    <b-nav-item v-bind:active="$route.path === '/conta/favoritos'" to="/conta/favoritos"><b-icon icon="star" /> Favoritos</b-nav-item>
-    <b-nav-item v-bind:active="$route.path === '/conta/playlists'" to="/conta/playlists"><b-icon icon="collection-play" /> Playlists</b-nav-item>
-    <b-nav-item @click="sair"><b-icon icon="power" /> Sair</b-nav-item>
-  </b-nav>
-</div>
+  <div>
+    <b-nav vertical>
+      <b-nav-item v-bind:active="$route.path === '/conta'" to="/conta"><b-icon icon="disc" /> Mus cds</b-nav-item>
+      <b-nav-item v-bind:active="$route.path === '/conta/meus-dados'" to="/conta/meus-dados"><b-icon icon="pencil-square" /> Meus dados</b-nav-item>
+      <b-nav-item v-bind:active="$route.path === '/conta/favoritos'" to="/conta/favoritos"><b-icon icon="star" /> Favoritos</b-nav-item>
+      <b-nav-item v-bind:active="$route.path === '/conta/playlists'" to="/conta/playlists"><b-icon icon="collection-play" /> Playlists</b-nav-item>
+      <b-nav-item @click="sair"><b-icon icon="power" /> Sair</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
